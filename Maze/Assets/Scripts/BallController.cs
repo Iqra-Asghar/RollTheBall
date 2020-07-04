b93ea98a20a1ec1dda39a9326fa2cedfe2ad76e2
Iqra Asghar <Iqrasgharr@gmail.com>
2020-07-04 19:57:23 +0500

    Maze Game
    Maze Game

---
+++ Maze/Assets/Scripts/BallController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+   
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float movevertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, movevertical);
+
+        rb.AddForce (movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("spawner"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
